c0f8b6859ce210626534b62d5332f0583b559580
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-09-13 22:04:02 -0300

    Added T=1 Q=10 happy case

---
+++ tests/caso_trivial/script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+./tp 4 1 10 $folder/input $folder/output
+ret=$?
+
+if [ $ret -ne 0 ]; then
+  fail "got a return of $ret"
+fi
+
+delta=`diff -q $folder/output $folder/expected_output`
+if [ "$delta" != "" ]; then
+  fail "unexpected output"
+fi
+
+pass
--- tp.cpp
+++ tp.cpp
@@ -1,13 +1,18 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv) {
   if (argc == 1) return 1;
 
-  int input_fd = open(argv[4], 0);
-  if (input_fd == -1) return 1;
+  std::ifstream input(argv[4], std::ios::in | std::ios::binary);
+  if (!input) return 1;
 
-  int output_fd = open(argv[5], 0);
-  if (output_fd == -1) return 1;
+  std::ofstream output(argv[5], std::ios::out | std::ios::binary);
+  if (!output) return 1;
+
+  if (input.peek() == EOF) return 0;
+
+  const char out[] = { 0x00, 0x00, 0x00, 0x00, 0x01, 0x50 };
+  output.write(out, 6);
+  output.close();
 }
