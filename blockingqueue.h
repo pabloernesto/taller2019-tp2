41b72250ee175b5747a24a935bec85a046c92873
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-10-08 00:53:56 -0300

    BlockingQueue: moved mutex and cv inside the class

--- blockingqueue.h
+++ blockingqueue.h
@@ -7,11 +7,10 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
-#include <memory>     // unique_ptr
 
 class BlockingQueue {
-  std::unique_ptr<std::mutex> mtx;
-  std::unique_ptr<std::condition_variable> empty_cv, full_cv;
+  std::mutex mtx;
+  std::condition_variable empty_cv, full_cv;
   std::queue<Record> q;
   size_t max_size;
   bool closed;
@@ -22,10 +21,6 @@ class BlockingQueue {
   // Non-copyable
   BlockingQueue(const BlockingQueue&) = delete;
   BlockingQueue& operator=(const BlockingQueue&) = delete;
-
-  // Move-constructable
-  BlockingQueue(BlockingQueue&& other);
-  BlockingQueue& operator=(BlockingQueue&& other);
 
   void push(Record&& x);
   Record pop();
